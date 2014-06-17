1bbb73f7c7fb6aa0bd7caf950024b7b3c06e6cda
CraigZheng <craignineten@gmail.com>
2014-06-17 17:51:57 +1000

    regular commit

---
+++ Build/Intermediates/ArchiveIntermediates/SocialComic2014/IntermediateBuildFilesPath/ZipArchive.build/Release-iphoneos/ZipArchive ios.build/Objects-normal/arm64/zip.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/craig/Developer/SocialComic2014/ZipArchive-master/minizip/zip.c \
+  /Users/craig/Developer/SocialComic2014/ZipArchive-master/minizip/zip.h \
+  /Users/craig/Developer/SocialComic2014/ZipArchive-master/minizip/ioapi.h \
+  /Users/craig/Developer/SocialComic2014/ZipArchive-master/minizip/crypt.h
