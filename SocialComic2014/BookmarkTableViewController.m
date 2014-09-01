4c80d708d827aa26d13f905b8719ff3a94b940ad
Craig Zheng <Craig@haozhengs-mbp.gateway>
2014-09-01 22:00:46 +1000

    added bookmark view controller

--- SocialComic2014/BookmarkTableViewController.m
+++ SocialComic2014/BookmarkTableViewController.m
@@ -13,15 +13,23 @@
 
 @interface BookmarkTableViewController ()
 @property NSMutableArray *bookmarks;
+@property BookmarkUtil *bookmarkUtil;
 @end
 
 @implementation BookmarkTableViewController
 @synthesize bookmarks;
+@synthesize bookmarkUtil;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    bookmarks = [NSMutableArray new];
+    bookmarkUtil = [BookmarkUtil getInstance];
+    if ([bookmarkUtil bookmarkedComics]) {
+        [bookmarks addObjectsFromArray:[bookmarkUtil bookmarkedComics]];
+    }
+    [self.tableView setContentInset:UIEdgeInsetsMake(20, 0, self.tabBarController.tabBar.frame.size.height, 0)];
+
 }
 
 #pragma mark - Table view data source
@@ -38,7 +46,12 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
     if (cell) {
         Comic *comic = [bookmarks objectAtIndex:indexPath.row];
-        
+        UIImageView *coverImageView = (UIImageView*) [cell viewWithTag:1];
+        UILabel *titleLabel = (UILabel*) [cell viewWithTag:2];
+        UILabel *descriptionLabel = (UILabel*) [cell viewWithTag:3];
+        coverImageView.image = comic.cover;
+        titleLabel.text = comic.name;
+        descriptionLabel.text = [NSString stringWithFormat:@"Bookmark at page %ld", (long) [bookmarkUtil bookmarkForComic:comic]];
     }
     return cell;
 }
